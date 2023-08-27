3e257ba9348af4a2bb6a42c5ce1e5aefcfbc71ec
Andrew Tullos <tullos.andrew@gmail.com>
2023-08-27 11:30:21 -0500

    JS Code started Psuedocode completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Script-Racer
+https://www.youtube.com/watch?v=-ZRDZyUjEEI&ab_channel=OpenJavaScript
+https://www.youtube.com/watch?v=4piMZDO5IOI&ab_channel=ReachStar
+https://www.w3schools.com/jsref/event_onclick.asp
+https://www.youtube.com/watch?v=8Nsb9cjmOVA&ab_channel=BroCode
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-// GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
@@ -7,5 +6,36 @@
 // THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
-// WHEN the game is over
 // THEN I can save my initials and score
+
+// Timer Function Psuedocode
+/*
+  Timer needs to span seconds and add over to minutes for every 60 second interval?
+
+  Activate the timer when pressing the start button with the id of start
+  Next create a way to add 10 seconds if a value is false class; or true class do nothing
+  When last question is pressed - log time to leaderboard page with option of initials 
+    - trigger this via last class
+
+
+*/
+let time = document.getElementById('timer');
+let startBtn = document.getElementById('start');
+let incorrectAnswer = document.querySelectorAll('false')
+let correctAnswer = document.querySelectorAll('true')
+let lastQuestionStopTime = document.querySelectorAll('last');
+
+let startTime = 0;
+let elapsedTime = 0;
+let hrs = 0;
+let minutes = 0;
+let sec = 0;
+
+// Timer
+startBtn.addEventListener('click', () => {
+  startTime = Date.now()
+})
+
+
+// Call the function to start the timer
+// startTimer();
